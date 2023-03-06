f3bd0ed82d73f6df7b7aa6285a0f1d78f0ab3bb2
GermanVelasco06 <83845085+GermanVelasco19@users.noreply.github.com>
2023-03-06 18:44:50 -0500

    Contactos

--- app/src/main/java/com/example/pawprints/participantesComunidad.kt
+++ app/src/main/java/com/example/pawprints/participantesComunidad.kt
@@ -1,5 +1,7 @@
 package com.example.pawprints
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.database.Cursor
 import android.os.Build.ID
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +12,8 @@ import android.provider.ContactsContract
 import android.widget.Button
 import android.widget.ListView
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import org.jetbrains.annotations.Contract
 
 class participantesComunidad : AppCompatActivity() {
 
@@ -17,6 +21,13 @@ class participantesComunidad : AppCompatActivity() {
     var mCursor: Cursor? = null
     var mContactsAdapter: ContactsAdapter? = null
     var mlista: ListView? = null
+
+    fun initView(){
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED){
+            mCursor=contentResolver.query(ContactsContract.Contacts.CONTENT_URI,mProjection,null,null,null)
+            mContactsAdapter?.changeCursor(mCursor)
+        }
+    }
 
     private fun requestContacsPermicion(){
         if (shouldShowRequestPermissionRationale(android.Manifest.permission.READ_CONTACTS)) {
@@ -37,7 +48,11 @@ class participantesComunidad : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_participantes_comunidad)
 
-        val mProjection = arrayOf(ContactsContract.Profile.ID,ContactsContract.Profile.DISPLAY_NAME_PRIMARY )
+        val mProjection = arrayOf(ContactsContract.Profile._ID,ContactsContract.Profile.DISPLAY_NAME_PRIMARY )
+
+        mContactsAdapter = ContactsAdapter(this,null,0)
+        mlista?.adapter = mContactsAdapter
+        initView()
 
 
     }
